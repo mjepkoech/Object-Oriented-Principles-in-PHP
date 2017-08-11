1e15ba5aa39cdb97c78cddea13dcfa73f593bb40
mjepkoech <mjepkoech@cytonn.com>
2017-08-11 05:45:18 +0000

    initial commit

---
+++ classes/main.php
@@ -0,0 +1,32 @@
+<?php
+function __autoload($class_name) 
+{
+    require_once “./class/class.”.$class_name.“.php”;
+}
+       
+//Creating an object of the class "teacher"
+$t1=new teacher();
+$t1->name="Ronnie";
+$t1->forename="Nyaga";
+$t1->email="@ronnie.com";
+
+echo $t1->name.", ".$t1->forename.": ".$t1->email;
+$t1->ID();
+
+//Creating an object of the class "student"
+$s1=new student();
+$s1->name="millie";
+$s1->forename="queen";
+
+echo $s1->name."; ".$s1->forename;
+$s1->ID();
+?>
+//person.php
+<?php
+class person
+{
+  //Attribute
+  public $name;
+  public $forename;
+}
+?>
